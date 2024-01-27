19ebbf81d0d944a95151c784014fb1d882debe10
Timothy Ryon <tim@timryon.com>
2024-01-27 15:30:12 -0500

    workingstateful search input

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+// function handleOnChange 
+
+function Filter({ onCategoryChange, searchChange, onSearchChange }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+      type="text" 
+      name="search" 
+      placeholder="Search..." 
+      onChange = { onSearchChange }
+      value = { searchChange }
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,6 +5,7 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchChange, onSearchChange] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -14,12 +15,22 @@ function ShoppingList({ items }) {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
+  }).filter(item => item.name.toLowerCase().includes(searchChange.toLowerCase())  
+  );
+
+  function handleSearchChange(event) {
+    onSearchChange(event.target.value);
+    console.log(event.target.value);
+  }
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+      onCategoryChange={handleCategoryChange} 
+      searchChange = { searchChange }
+      onSearchChange= { handleSearchChange }
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
